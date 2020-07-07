cbf4571a5882d4b8cd159ae23497f0eca9671be7
Niklas Degener (Surface) <deg.niklas@web.de>
2020-07-07 17:00:05 +0200

    Fix, now its working

--- main.hs
+++ main.hs
@@ -1,30 +1,23 @@
 import Debug.Trace
 import Data.Ratio
 
---main = do
-  --putStrLn rootn 5
-
 rootSin :: Rational -> Rational -> Rational -> Rational
 rootSin val cur factor =
   if (cur*cur == val) then
       cur
     else
       if (cur*cur < val) then
-      --trace ("Current Number = " ++ show cur) $
       rootSin val (cur+(1/factor)) factor
     else
       cur-1/factor
 
-rootMult:: Rational -> Rational -> Rational -> Rational -> Rational
-rootMult n it maxIt akku = if (it==maxIt) then
-   akku
+rootMult:: Rational -> Integer -> Integer -> Rational -> Double
+rootMult n it maxIt akku = if (it==maxIt+1) then
+   fromRational(akku)
    else
-     --trace("Current akku = " ++ show akku) $
-     rootMult n (it+1) maxIt (rootSin n akku (it*10))
+     rootMult n (it+1) maxIt (rootSin n akku (10^it))
 
-rootn n = rootSin n 0 10
-rootnn n m l = rootSin n m l
-rootm n = rootMult n 1 100 0
+root n m = rootMult n 1 m 0
 
 fac n = if n == 0 then 1 else n * fac (n - 1)
 
