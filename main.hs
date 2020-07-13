6f4fec8fa651ec057363c1f19fa63feac2a3a350
Niklas Degener (Surface) <deg.niklas@web.de>
2020-07-14 01:58:23 +0200

    Now working efficiently for inputs >= 1

--- main.hs
+++ main.hs
@@ -1,6 +1,5 @@
 import Debug.Trace
 import Data.Ratio
-import Data.NumberLength
 
 rootSin :: Rational -> Integer -> Rational -> Rational -> Rational
 rootSin val pot cur factor =
@@ -8,15 +7,20 @@ rootSin val pot cur factor =
       cur
     else
       if (cur^pot < val) then
-      rootSin val pot (cur+(1/factor)) factor
+        rootSin val pot (cur+((10^((rationalLength val 0)-1))/factor)) factor
     else
-      cur-1/factor
+      cur-((10^((rationalLength val 0)-1))/factor)
 
 rootMult:: Rational -> Integer -> Integer  -> Integer -> Rational -> Rational
 rootMult n pot it maxIt akku = if (it==maxIt+1) then
-    trace("Länge: " ++ lengthInteger(akku))
     akku
    else
      rootMult n pot (it+1) maxIt (rootSin n pot akku (10^it))
 
-root n pot m = rootMult n pot 1 m 0
+root n pot m = rootMult n pot 0 (m+(rationalLength n 0)-1) 0
+
+rationalLength:: Rational -> Integer -> Integer
+rationalLength rat res = if (rat < 10) then
+    res+1
+  else
+    rationalLength (rat/10) (res+1)
