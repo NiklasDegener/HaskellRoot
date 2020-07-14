8585988a2500cd6a3cc97f7fe6ee12bb4b785b4c
Niklas Degener (Surface) <deg.niklas@web.de>
2020-07-14 15:48:27 +0200

    Added documentation

--- main.hs
+++ main.hs
@@ -1,6 +1,7 @@
 import Debug.Trace
 import Data.Ratio
 
+--calculates a single digit of a root
 rootSin :: Rational -> Integer -> Rational -> Rational -> Rational
 rootSin val pot cur factor =
   if (cur^pot == val) then
@@ -11,14 +12,17 @@ rootSin val pot cur factor =
     else
       cur-((10^((rationalLength val 0)-1))/factor)
 
+--calculates a complete root of an abritary number with arbitrary precision
 rootMult:: Rational -> Integer -> Integer  -> Integer -> Rational -> Rational
 rootMult n pot it maxIt akku = if (it==maxIt+1) then
     akku
    else
      rootMult n pot (it+1) maxIt (rootSin n pot akku (10^it))
 
+--n: Number you want to take the root from; pot: the power of the root; m: precision (amount of digits after comma)
 root n pot m = rootMult n pot 0 (m+(rationalLength n 0)-1) 0
 
+--length of a rational number represented as an Integer value
 rationalLength:: Rational -> Integer -> Integer
 rationalLength rat res = if (rat < 10) then
     res+1
